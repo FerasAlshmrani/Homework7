74f69b9a732ad29b1174c1d324b8a1f09131a022
FerasAlshmrani <101283810+FerasAlshmrani@users.noreply.github.com>
2023-07-31 15:21:24 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,31 @@
+public class Employee {
+    private String id;
+    private String name;
+    private int salary;
+
+    Employee(String id , String name, int salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int AnnualSalary(){
+        return salary*12;
+    }
+    public int raisedSalary(int percent){
+        salary *= percent;
+        return salary;
+    }
+
+    public String toString() {
+        return "Employee name : "+ name+ " ,Employee id : "+ id + " ,Employee salary "+ salary;
+    }
+}
+
